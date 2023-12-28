5e1759cfe911798f04f3e209ad5bc0acfde8b689
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-12-28 16:29:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_portfolio/res/constants.dart';
-import 'package:flutter_portfolio/view/splash/splash_view.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:web_portfolio/pages/home/home.dart';
+import 'package:web_portfolio/utils/constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Flutter Portfolio",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        scaffoldBackgroundColor: bgColor,
-        useMaterial3: true,
-        textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white,)
-            .copyWith(
-          bodyText1: const TextStyle(color: bodyTextColor),
-          bodyText2: const TextStyle(color: bodyTextColor),
+      themeMode: ThemeMode.dark,
+      darkTheme: Theme.of(context).copyWith(
+        platform: TargetPlatform.android,
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        canvasColor: kBackgroundColor,
+        textTheme: GoogleFonts.latoTextTheme(),
+      ),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget),
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.resize(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: TABLET),
+          ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          ResponsiveBreakpoint.resize(2460, name: "4K"),
+        ],
+        background: Container(
+          color: kBackgroundColor,
         ),
       ),
-
-      home: SplashView()
+      home: Home(),
     );
   }
 }
-
-
